8a6f2510f39619df2cd3606001bf78ecc23ffb8a
Tony Bellavia <at.bellavia@gmail.com>
2020-02-15 02:09:03 +0100

    adding timer to evaluate time performance for the algorithm

--- solver.py
+++ solver.py
@@ -1,4 +1,5 @@
 from string import ascii_lowercase
+import time
 
 FILE_PREFIX = "letters"
 
@@ -19,12 +20,22 @@ def get_letter(ascii_art_letter):
 
 def test_challenge01():
 	counter = 0
+	begin = time.time()
+	# Testing time
+	for letter in ascii_lowercase:
+		get_letter(get_ascii_letter(letter))
+	end_time = time.time() - begin
+
+	# Testing correct answer
 	for letter in ascii_lowercase:
 		if letter == get_letter(get_ascii_letter(letter)):
 			counter += 1
 			print(f"{letter} -> [{OKGREEN}✓{ENDC}]")
 		else:
 			print(f"{letter} -> [{FAIL}x{ENDC}]")
+
 	print(f"Total : {counter} / {len(ascii_lowercase)}")
+	print(f"Executed in : {end_time:.5f}s")
+
 
 test_challenge01()
